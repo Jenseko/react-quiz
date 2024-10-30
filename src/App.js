a0e5ae601216e5ae4badf2062519893b1ba958bf
Jens Kötschau <123948185+Jenseko@users.noreply.github.com>
2024-10-30 11:07:38 +0100

    create own snippet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header.js";
+import Main from "./Main.js";
 
-export default function App () {
-  return <div>
-    <DateCounter />
-  </div>
-};
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main />
+    </div>
+  );
+}
